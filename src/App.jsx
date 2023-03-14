5e8172b9065c5ee8d328a19653afd90e820e4a5e
suryakumarSK4 <suryakumar.a@codingmart.com>
2023-03-14 17:53:39 +0530

    My candidate completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import Router from "./pages/Router";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import FormikConcepts from "./components/FormikConcepts/FormikConcepts";
 
 function App() {
   return (
     <BrowserRouter>
       <Router />
-      <ToastContainer />
+      {/* <FormikConcepts /> */}
     </BrowserRouter>
   );
 }
